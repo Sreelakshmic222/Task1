fd56effa5b9f476ddcd5652a782160f01ce12b91
Sriiiilraa <sreelakshmic222@gmail.com>
2023-04-22 22:22:56 +0530

    Comminting

--- restapi/urls.py
+++ restapi/urls.py
@@ -22,7 +22,7 @@ from api.views import HomePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api',include("api.urls")),
+    path('api/',include("api.urls")),
     path('', HomePageView.as_view(), name='home'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
